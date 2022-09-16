d3a085b10a65d785ac8011c5422748f1f8b84bcc
Stefan Baranoff <stefan.baranoff@trinitycyber.com>
2022-09-16 17:38:36 -0400

    remove need for installed python36 package

--- run-portal-sync.sh
+++ run-portal-sync.sh
@@ -4,6 +4,7 @@ if [[ -z $instance ]]; then
     instance=default
 fi
 
+cd /opt/trinity/tc-portal-sync
 source /opt/trinity/tc-portal-sync/runtime/bin/activate
 source /opt/trinity/tc-portal-sync/environment-$instance
 python /opt/trinity/tc-portal-sync/tc_portal_sync.py --config /opt/trinity/tc-portal-sync/config-${instance}.json ${@:2}
